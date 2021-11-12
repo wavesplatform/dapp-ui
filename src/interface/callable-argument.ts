02effe4606e17f31d22c2358e0a1f2d72ab3ae1f
inazarov <inazarov@web3tech.ru>
2021-11-12 12:51:33 +0300

    node api types

--- src/interface/callable-argument.ts
+++ src/interface/callable-argument.ts
@@ -1,7 +1,10 @@
-export type ICallableArgumentType = 'Int' | 'String' | 'ByteVector' | 'Boolean'
+import { TCallableFuncArgumentsArray } from '@waves/node-api-js';
 
-export type TCallableFuncArgumentsArray = { name: string, type: ICallableArgumentType }[]
-export type TCallableFuncArgumentsRecord = Record<string, ICallableArgumentType>
-export type TCallableFuncArguments = TCallableFuncArgumentsArray | TCallableFuncArgumentsRecord 
+export type {
+    ICallableFuncArgumentType as ICallableArgumentType,
+    TCallableFuncArgumentsArray,
+    TCallableFuncArgumentsRecord,
+    TCallableFuncArguments,
+} from '@waves/node-api-js';
 
 export type ICallableFuncTypesArray = Record<string, TCallableFuncArgumentsArray>
