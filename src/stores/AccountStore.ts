8f86f22c87ec9c47f95999b41ce817388b7a7e1d
Boris Volkov <bvolkov@web3tech.ru>
2024-03-04 17:18:06 +0300

    Fix miss IssueTransaction in balance

--- src/stores/AccountStore.ts
+++ src/stores/AccountStore.ts
@@ -8,6 +8,7 @@ import {RootStore} from '@stores/RootStore';
 import { ELoginType } from '@src/interface';
 
 import {SubStore} from './SubStore';
+import { balance } from '@waves/waves-transactions/dist/nodeInteraction';
 
 class AccountStore extends SubStore {
     @observable assets: { [name: string]: IAsset } = {'WAVES': {name: 'WAVES', assetId: 'WAVES', decimals: 8}};
@@ -37,6 +38,26 @@ class AccountStore extends SubStore {
         const path = `${checkSlash(server)}assets/balance/${address}`;
         const resp = await fetch(path);
         const data = (await (resp).json());
+
+        const assetsIds = data.balances.map((i: any) => !i.issueTransaction ? i.assetId : null).filter(Boolean);
+
+        if (assetsIds.length) {
+            const pathAssets = `${checkSlash(server)}assets/details`;
+            const respAssets = await fetch(pathAssets, { method: 'POST', body: JSON.stringify({ ids: assetsIds }), headers: { 'accept': 'application/json', 'Content-Type': 'application/json' } });
+            const additionAsset = (await (respAssets).json()).reduce((acc: any, asset: any) => {
+                acc[asset.assetId] = asset;
+                return acc;
+            }, {});
+            data.balances = data.balances
+            .map((balance: any) => ({ ...balance, issueTransaction: additionAsset[balance.assetId] }))
+            .filter((balance: any) => { 
+                if (!balance.issueTransaction) {
+                    console.warn(`Not found asset for balance ${balance.assetId}`);
+                    return false;
+                }
+                return true;
+             });
+        }
 
         const nftResp = await fetch(`${checkSlash(server)}assets/nft/${address}/limit/1000`);
         const nft: { 'assetId': 'string', 'name': 'string', 'decimals': 0 }[] = (await (nftResp).json());
